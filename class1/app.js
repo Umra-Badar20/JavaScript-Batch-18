c48726f5e44ba36784d24f2e11c6cf769e499f2c
Umra-Badar20 <umrabadar555@gmail.com>
2025-12-19 16:00:15 +0500

    Chap 2

---
+++ class1/app.js
@@ -0,0 +1,19 @@
+// alert( "Umra \n Badar" );
+// window.alert("Hello world")
+// alert(5)
+
+var firstName = "657"
+console.log(firstName); 
+console.log("firstName")
+
+// alert(firstName)
+
+
+firstName = "Umra \n badar"
+console.log(firstName); 
+
+document.write(firstName)
+alert(firstName)
+var lastName;
+
+
