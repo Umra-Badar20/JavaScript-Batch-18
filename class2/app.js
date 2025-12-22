3c566fb978e1031945a33bdc9baa9a82400b00a3
Umra-Badar20 <umrabadar555@gmail.com>
2025-12-22 16:06:43 +0500

    Chap 6 half

---
+++ class2/app.js
@@ -0,0 +1,61 @@
+// alert("Umra")
+// var name = "Umra"
+// var degree = "BSSE"
+// var salary = 100000
+// console.log( "Welcome " + name + "! to our website. You are the student of " + degree+ ". Your salary is "+ salary);
+
+
+// var num1 = 10
+// var num2 = "20"
+// var num3 = num1+num2
+// console.log(num3);
+// console.log(num1+num2); //1020
+// var num = 10
+// num = num + 10
+// console.log(num);
+
+// var alert = "abc" //not allowed
+// console.log(alert);
+
+
+// var num = 2+2
+// console.log(num);
+// console.log( "2 plus 2 equals " +2+2); // 4 equals 4
+
+
+//___________Increment
+// var num = 10
+// // num = num + 1
+// num++  //11
+// num++
+// console.log(num++); //10 //11
+// console.log(num);//11  //12
+
+
+
+
+
+
+//_____________Decrement
+
+// var num = 10
+// num--
+// console.log(num--); //9  8
+// console.log(num--); //8  7
+// num++  //7  8
+// console.log(num); //8
+
+var a = 5
+var b = 2
+
+// var c = a++ + a + b++ + b
+       //5 +  6 + 2 + 3 
+
+
+
+       //6    5     6      3     2    6
+var c = a++ + a-- + a++ + b++ + b-- + a + b  //29
+        //5 + 6 +   5 +   2 +  3 + 6 + 2 
+console.log(c);
+
+
