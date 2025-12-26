1c9879d2ec133e13ed5cc80d55b00d257eb09aff
Umra-Badar20 <umrabadar555@gmail.com>
2025-12-26 16:06:48 +0500

    chap 11

---
+++ class4/app.js
@@ -0,0 +1,40 @@
+var a = 10
+// console.log(a++ , a++ ,a) ;
+// console.log(a-- , a-- , a);
+
+// console.log(++a , ++a , a);
+// console.log(--a , --a, a);
+
+// var num = +prompt("enter a number")
+// var num1 = +prompt("enter a number")
+// var num2 = +prompt("enter a number")
+// console.log(num + num1+ num2 );
+
+
+// console.log("Before block");
+
+// if(2 === "2"){
+//     console.log("Hello");
+// }
+
+
+// console.log("After block");
+
+var ans = "html"
+var ques =  prompt("How can we create a structure of website?")
+var score = 0
+
+if(ques === ans){
+    score++
+    console.log("Correct answer!" );
+
+}else{
+    console.log("wrong answer!" );
+}
+// if(ques!==ans){
+//     console.log("wrong Answer");
+    
+// }
+console.log("Your score is: "+score);
+
+
