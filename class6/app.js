cd5413bfa3ced076815997a7be9ad6b962472410
Umra-Badar20 <umrabadar555@gmail.com>
2025-12-31 15:57:29 +0500

    chap 14

---
+++ class6/app.js
@@ -0,0 +1,70 @@
+// var userName = prompt("Enter you name")
+
+// if(userName !== ""){
+//     console.log(userName);   
+// }else{
+//     console.log("Name not provided");    
+// }
+
+// if(userName){
+//     console.log(userName);   
+// }else{
+//     console.log("Name not provided");    
+// }
+
+
+// //Falsy Values 
+// console.log(!!NaN);
+// console.log(!!0);
+// console.log(!!false);
+// console.log(!!"");
+// console.log(!!undefined);
+// console.log(!!null);
+// console.log(Boolean(null));
+
+
+
+// if("html" || "bootstrap" || ("javascript" && "css")){
+
+// }
+// var language1 = prompt("do you know javascript yes or no");
+// var language2 = prompt("do you know css yes or no");
+// var language3 = prompt("do you know html yes or no");
+// var language4 = prompt("do you know bootstrap yes or no");
+
+// if((language1 == "yes" && language2 == "yes")){
+//    if(language3 === 'yes'|| language4 === 'yes'){
+//     console.log('you are eligible')
+//    }
+//    else{
+//     console.log('you are  eligible')
+//    }   
+// }
+// else{
+//     console.log("you are not eligible");    
+// }
+
+
+var js = prompt("Js aati h?")
+
+if(js == "yes"){
+var exp = prompt("Exp kitna h?")
+
+    if(exp >= 2){
+        
+        
+var city = prompt("kahan se ho?")
+
+        if(city === "karachi"){
+            console.log("Ajao kl se");
+            
+        }else{
+            console.log("Karachi ajao");          
+        }
+    }else{
+        console.log("bary ho k aao");
+        
+    }
+}else{
+    console.log("js seekh k aao"); 
+}
