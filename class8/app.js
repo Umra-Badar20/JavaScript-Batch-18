107d5fb4d8cb97bf7b6158f3d1b2c06b4adeb123
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-05 15:59:11 +0500

    chap 16

---
+++ class8/app.js
@@ -0,0 +1,37 @@
+// var arr = [ "Umra", "Hanifa", true, 10]
+// var poped = arr.pop() //Returns Removed element
+// var pushed = arr.push("Areeba") //returns array's updated length
+// // console.log(poped);
+
+// var shifted = arr.shift() //Returns Removed element
+// var unshifted = arr.unshift("iqra",19,2) //returns array's updated length
+// console.log(arr, poped,pushed,shifted,unshifted);
+
+
+// LIFO  push shift
+// FIFO  unshift  shift
+
+
+// var fruits = ["apple", "mango", "banana", "orange", "grapes","cherry"]
+// // var spliced = fruits.splice(1,0,"kiwi","cherry",1)
+// // console.log(fruits, spliced);
+
+// var slicedArr = fruits.slice(0,1)
+// console.log(fruits);
+// console.log(slicedArr);
+
+// var fruits = ["apple", "mango", "banana", "orange", "grapes","cherry"]
+// var newfruits = fruits.toReversed();
+// console.log(newfruits);
+
+// console.log(fruits.sort());
+
+// var fruits = ["apple", "mango", "banana", "orange", "grapes","cherry"]
+// console.log(fruits.join(" "));
+
+
+// var numbers = [9,7,4,10, 11,3,2]
+// console.log(numbers.sort());
+
+var multi_arr = [[1,2,3],[4,5,[6]],[7,8,9],10]
+console.log(multi_arr[1][2][0]);
