4afa977cc185ce2ad0c94d92fc501aa824f9406b
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-09 16:06:40 +0500

    chap 18

---
+++ class10/app.js
@@ -0,0 +1,59 @@
+//initialization ; condition ; increment/decrement
+// var i;   //10<10
+// for(i = 1; i<10 ; i++){
+//     document.write(i+"</br>")
+//     document.write("Umra Badar </br>")
+// }
+// document.write("Loop ended")
+
+//1
+//2
+//3
+//4
+// 5
+// 6
+// 7
+// 8
+//9
+
+var students = ["student1", "student2", "student3", "student4", "student5"];
+// document.write(`Welcome ${students[0]} to SMIT <br>`)
+// document.write(`Welcome ${students[1]} to SMIT<br>`)
+// document.write(`Welcome ${students[2]} to SMIT<br>`)
+// document.write(`Welcome ${students[3]} to SMIT<br>`)
+// document.write(`Welcome ${students[4]} to SMIT<br>`)
+
+//             5<=5
+// for(var i = 0; i< students.length ; i++){
+//     document.write(`Welcome ${students[i]} to SMIT <br>`)
+// }
+
+// var arr = [2,5,11,3,7]
+
+// for(var i = 0 ; i < arr.length ; i++){
+//     // document.write(arr[i]+ " x 5 = " + arr[i]*5 +"<br>")
+//     document.write(`${arr[i]} x 5 = ${arr[i]*5} <br>`)
+// }
+// 2 x 5 = 10
+// 5 x 5 = 25
+// 11 x 5 = 55
+
+// var val = +prompt("Enter a number for table:", 5) || 5;
+// var len = +prompt("Enter a length for table:", 10) || 10;
+// for (var i = 1; i <= len; i++) {
+//   document.write(val + " x " + i + " = " + val * i + "<br>");
+// }
+
+
+// var cities = ["karachi","lahore", "multan","sawat"]
+// var userCity = prompt("Enter your city name:")
+// // userCity = userCity.toLowerCase()
+// for(var i = 0; i<cities.length; i++){
+//     if(userCity.toLowerCase() === cities[i]){
+//         document.write("Congratulations! You are selected")
+//     }
+// }
+for(var i =10; i>0; i--){
+    console.log(i);
+    
+}
