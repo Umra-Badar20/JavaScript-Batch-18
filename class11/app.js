96c6cf87174a4d685d9888b8af54db06bd91c1e7
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-14 14:42:30 +0500

    chap 20

---
+++ class11/app.js
@@ -0,0 +1,47 @@
+// for(var i = 0; i<5 ; i++){
+//     console.log("Umra");
+// }
+
+// var arr = ["UmRa","Areeba","iqra","tanzeela","Fizza"]
+// var findName = "fizza";
+// var flag = false
+
+// for(var i=0; i<arr.length; i++){
+//     // console.log(arr[i].toLowerCase());
+//     if(findName.toLowerCase() === arr[i].toLowerCase()){
+//         flag= true
+//         console.log(findName+ " Found in array at index "+ i);
+//         break;
+//     }
+// }
+// console.log(flag);
+
+// if(!flag){
+//     console.log(findName+ " not Found in array");
+// }
+
+// for(var i = 1; i<=10; i++){
+//     if(i===5){
+//         console.log(i)
+//         break;
+//     }
+// }
+
+// for(var i=0; i<5; i++){
+
+//     for(var j=0; j<5; j++){
+//         console.log(i,j);
+//     }
+// }
+
+for (var i = 1; i < 5; i++) {//i=5
+  for (var j = 1; j < 5; j++) {//j=1
+    document.write("*");
+  }
+  document.write("<br>");
+}
+// ****
+// ****
+// ****
+// ****
+
