ad91e7350e36aa7b00dcca7bf19324e22be8a51f
Umra-Badar20 <umrabadar555@gmail.com>
2026-01-21 15:50:52 +0500

    math methods

---
+++ class15/index.js
@@ -0,0 +1,36 @@
+// console.log(Math.round(45.1));  
+
+//  console.log(Math.ceil(45.1)); // 46
+
+//   console.log(Math.floor(45.6));  
+    
+// var a = Math.floor(Math.random() * 2)
+// console.log(a);
+
+
+// var b= "67"
+// console.log(parseInt(b)); // point remove
+// console.log(parseFloat(b));   // with decimal
+
+// var bInNo = Number(b);
+
+
+// var randomNo = 378456
+// console.log(randomNo.toString() + 2);
+
+
+var randomNo = 37.8456
+// console.log(randomNo.toFixed(2)); // 37.84
+
+
+ 
+
+
+
+
+
+
+
+
+
+
