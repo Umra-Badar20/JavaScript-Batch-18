4d355cb493d8d88ac2c324202d9ee7960103ca29
hiraekram10 <heraiqram123@gmail.com>
2026-02-18 15:55:10 +0500

    chap 46

---
+++ class25/app.js
@@ -0,0 +1,16 @@
+function greet() {
+  var msg = "Welcome to SMIT!";
+  console.log(msg);
+}
+function carClicked(e){
+    // alert("Car clicked")
+    console.log(e);
+    e.src = "https://assets-v2.lottiefiles.com/a/49847160-3f53-11ef-8109-cbcc69139eb2/fZaPRJe15G.gif"
+    
+}
+function sum(num1){
+    console.log(num1+ num1);
+    
+}
+sum(2)
+sum("a")
