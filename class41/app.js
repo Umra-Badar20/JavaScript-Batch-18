9af94f6ec9d3fece96912b443a197bd764615bc0
Umra-Badar20 <umrabadar555@gmail.com>
2026-04-20 16:05:33 +0500

    Chap 80

--- class41/app.js
+++ class41/app.js
@@ -5,7 +5,7 @@ function visitLink(){
     window.location.assign("https://lms.saylanimit.com/trainer/")
 }
 
-// var newWindow = window.open()
-// var content = "<h1>Umra BAdar</h1>"
-// newWindow.document.write(content)
+var newWindow = window.open()
+var content = "<h1>Umra BAdar</h1>"
+newWindow.document.write(content)
 window.open("", "win1", "width=420,height=380,left=200,top=100")
